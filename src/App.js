f7be2641295f43c878638c17a073c8ccd51a697a
stepancar <stepancar@hotmail.com>
2021-01-26 21:12:48 +0300

    feat(*): set al-khwarizmi

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './al-khwarizmi.jpg';
 import './App.css';
 
 function App() {
@@ -6,16 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          HIHI-HAHA
         </a>
       </header>
     </div>
