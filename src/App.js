55945c89d12229115d9a6aac8cebb23b90dc5222
stepancar <stepancar@hotmail.com>
2021-01-28 21:47:23 +0300

    fix(*): update hihi haha

--- src/App.js
+++ src/App.js
@@ -5,15 +5,21 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          <span className="Name">Хихи хаха клуб</span>
+          - клуб без обязательств, где мы решаем задачки и пиздим с пацанами на позитиве
+        </h1>
+        <a href="https://ru.wikipedia.org/wiki/%D0%90%D0%BB%D1%8C-%D0%A5%D0%BE%D1%80%D0%B5%D0%B7%D0%BC%D0%B8">
+          <img src={logo} className="App-logo" alt="logo" />
+        </a>
         
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://t.me/joinchat/IdBhMiKdNEt64TT6"
           target="_blank"
           rel="noopener noreferrer"
         >
-          HIHI-HAHA
+          Присоединяйся к ХИХИ-ХАХА
         </a>
       </header>
     </div>
