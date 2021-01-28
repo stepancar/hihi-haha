c40a30b79fe6568661cbb5243beaec82ba4c1840
stepancar <stepancar@hotmail.com>
2021-01-28 21:48:43 +0300

    fix(*): update hihi haha

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           - клуб без обязательств, где мы решаем задачки и пиздим с пацанами на позитиве
         </h1>
         <a href="https://ru.wikipedia.org/wiki/%D0%90%D0%BB%D1%8C-%D0%A5%D0%BE%D1%80%D0%B5%D0%B7%D0%BC%D0%B8">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="Аль-Хорезми" />
         </a>
         
         <a
